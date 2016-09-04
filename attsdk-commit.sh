8f60941174350ab29a221f769316b6c1f2938ebc
attsdk <att.drt.sdk@gmail.com>
2016-09-03 22:25:11 -0700

    Add pushing changes to remote

--- attsdk-commit.sh
+++ attsdk-commit.sh
@@ -10,17 +10,28 @@ __author="attsdk"
 
 arg1="${1:-}"
 
+function print_error {
+ 	__message="${1:-}"
+ 
+ 	tput setaf 1;
+ 	echo "${__message}"
+ 	tput sgr0;
+}
+
+function print_message {
+	 __message="${1:-}"
+
+	tput setaf 2;
+	echo "${__message}"
+	tput sgr0;
+}
+
 function check_params {
   __commit_message="${1:-}"
 
   if [[ -z ${__commit_message} ]]; then
-    tput setaf 1;
-    echo "Error: Missing params, please check usage!"
-    tput sgr0;
-
-    tput setaf 2;
-    echo "${__usage:-}"
-    tput sgr0;
+    print_error "Error: Missing params, please check usage!"
+    print_message "${__usage:-}"
     
     exit 1
   fi
@@ -32,11 +43,15 @@ function check_params {
 
 check_params ${arg1}
 
-if ! git commit --author "${__author} <${_email}>" -m "${arg1}" ; then
-  tput setaf 1;
-	echo "Error: Failed to commit changes"
-	tput sgr0;
+if ! git commit --author "${__author} <${__email}>" -m "${arg1}" ; then
+	print_error "Error: Failed to commit changes"
 
 	exit 1
+fi
+
+if ! git push ; then
+	print_error "Error: Failed to push changes "
+	print_message "Fix the issue and try to push changes manually"
+	echo "git push"
 fi
 
