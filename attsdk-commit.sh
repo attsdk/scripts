a4741560583619542a43fe337b1539c7749fe4d0
attsdk <att.drt.sdk@gmail.com>
2016-09-03 23:16:09 -0700

    Add repo to commit as optional parameter

--- attsdk-commit.sh
+++ attsdk-commit.sh
@@ -4,11 +4,17 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-__usage="USAGE: $ ./attsdk-commit.sh COMMIT_MESSAGE"
+__usage="USAGE: $ ./attsdk-commit.sh COMMIT_MESSAGE [REPO_DIR]"
 __email="att.drt.sdk@gmail.com"
 __author="attsdk"
+__src_dir=$(pwd)
 
-arg1="${1:-}"
+COMMIT_MSG="${1:-}"
+REPO_DIR="${2:-}"
+
+###############
+## FUNCTIONS ##
+###############
 
 function print_error {
  	__message="${1:-}"
@@ -26,10 +32,10 @@ function print_message {
 	tput sgr0;
 }
 
-function check_params {
+function check_required_params {
   __commit_message="${1:-}"
 
-  if [[ -z ${__commit_message} ]]; then
+  if [[ -z ${__commit_message} ]] ; then
     print_error "Error: Missing params, please check usage!"
     print_message "${__usage:-}"
     
@@ -37,21 +43,38 @@ function check_params {
   fi
 }
 
+function exit_program {
+	echo "Switching back to ${__src_dir}"
+	echo "Exiting..."
+
+	cd ${__src_dir}
+	exit 1
+}
+
 ##########
 ## MAIN ##
 ##########
 
-check_params ${arg1}
+check_required_params ${COMMIT_MSG}
 
-if ! git commit --author "${__author} <${__email}>" -m "${arg1}" ; then
+if [[ -z ${REPO_DIR} ]] ; then
+	echo "Current directory is the working directory"
+else
+	echo "Switching to repository directory ${REPO_DIR}"
+	cd ${REPO_DIR}	
+fi
+
+if ! git commit --author "${__author} <${__email}>" -m "${COMMIT_MSG}" ; then
 	print_error "Error: Failed to commit changes"
 
-	exit 1
+	exit_program
 fi
 
 if ! git push ; then
-	print_error "Error: Failed to push changes "
+	print_error "Error: Failed to push changes"
 	print_message "Fix the issue and try to push changes manually"
 	echo "git push"
+
+	exit_program
 fi
 
